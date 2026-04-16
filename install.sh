dffc784a781547ac5d807a9f1143a283c9838cff
Satoshi <burntphone73@gmail.com>
2026-04-16 08:14:54 -0500

    Add argon2-cffi to xrp dependencies installation

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Installing xrp dependencies..."
-pip3 install xrpl-py rich qrcode cryptography --break-system-packages -q
+pip3 install xrpl-py rich qrcode cryptography argon2-cffi --break-system-packages -q
 
 echo "Installing xrp to /usr/local/bin..."
 sudo cp xrp /usr/local/bin/xrp
